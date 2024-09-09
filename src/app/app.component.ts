8ef886830c16b915d2f053c549691a4849e28f65
Mostafa  Fawzy <Moustafa_20210944@fci.helwan.edu.eg>
2024-09-09 13:09:50 +0300

    Added and updated components, enhanced layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+import { BioComponent } from './components/bio/bio.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ProtfolioComponent } from './components/protfolio/protfolio.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
+//Decorator
 @Component({
+  //component directive
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BioComponent,HeroComponent,ProtfolioComponent,FooterComponent,SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'myapp';
 }
